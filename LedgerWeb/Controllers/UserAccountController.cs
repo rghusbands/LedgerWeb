13bd298f9e7374a592ec0b6f038e8b672432b57e
rghusbands <rghusbands@comcast.net>
2017-11-27 05:42:34 -0800

    Initial commit

---
+++ LedgerWeb/Controllers/UserAccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LedgerWeb.Models;
+
+namespace LedgerWeb.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        // GET: UserAccount
+        public ActionResult Random()
+        {
+            var user = new UserAccount() { Name = "Bob" };
+
+            return View(user);
+        }
+    }
+}
